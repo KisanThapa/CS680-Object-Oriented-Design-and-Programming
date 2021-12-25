c24b8210655effcb7ec3ef471e50f0a85dc782d2
Kisan Thapa <kisanthapa33@gmail.com>
2021-12-25 16:57:45 -0500

    delete unnecessary out folder

---
+++ hw11/src/edu/umb/cs680/hw10/apfs/ApfsLink.java
@@ -0,0 +1,44 @@
+package edu.umb.cs680.hw10.apfs;
+
+import edu.umb.cs680.hw10.filesystem.FSElement;
+
+import java.time.LocalDateTime;
+
+public class ApfsLink extends ApfsElement {
+
+    private final FSElement target;
+
+    public ApfsLink(ApfsDirectory parent, String name, int size, LocalDateTime creationTime, FSElement target, String ownerName, LocalDateTime lastModifiedTime) {
+        super(parent, name, size, creationTime, ownerName, lastModifiedTime);
+        this.target = target;
+        
+        parent.appendChild(this);
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return false;
+    }
+
+    public FSElement getTarget() {
+        return target;
+    }
+
+    @Override
+    public String toString() {
+        return "ApfsLink{" +
+                "ownerName='" + ownerName + '\'' +
+                ", lastModifiedTime=" + lastModifiedTime +
+                ", target=" + target +
+                ", parent=" + parent +
+                ", name='" + name + '\'' +
+                ", size=" + size +
+                ", creationTime=" + creationTime +
+                '}';
+    }
+
+    @Override
+    public void accept(ApfsFSVisitor visitor) {
+        visitor.visit(this);
+    }
+}
---
+++ hw11/test/src/edu/umb/cs680/hw10/apfs/APFSTest.java
@@ -0,0 +1,58 @@
+package edu.umb.cs680.hw10.apfs;
+
+import edu.umb.cs680.hw10.apfs.APFS;
+import edu.umb.cs680.hw10.apfs.ApfsDirectory;
+import edu.umb.cs680.hw10.apfs.ApfsFile;
+import edu.umb.cs680.hw10.apfs.ApfsLink;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class APFSTest {
+
+    static LocalDateTime localTime = LocalDateTime.now();
+    static APFS apfs;
+    static ApfsDirectory root;
+
+    @BeforeAll
+    public static void setupTest() {
+        apfs = APFS.getAPFS();
+
+        // Root Directories
+        root = (ApfsDirectory) apfs.initFileSystem("root", 1200);
+
+        // Directories
+        ApfsDirectory applications = new ApfsDirectory(root, "applications", 0, localTime, "owner1", localTime);
+        ApfsDirectory home = new ApfsDirectory(root, "home", 0, localTime, "owner1", localTime);
+        ApfsDirectory code = new ApfsDirectory(home, "code", 0, localTime, "owner1", localTime);
+
+        // Files
+        ApfsFile a = new ApfsFile(applications, "a", 2, localTime, "owner1", localTime);
+        ApfsFile b = new ApfsFile(home, "b", 2, localTime, "owner1", localTime);
+        ApfsFile c = new ApfsFile(code, "c", 2, localTime, "owner1", localTime);
+        ApfsFile d = new ApfsFile(code, "d", 2, localTime, "owner1", localTime);
+
+        // Links
+        ApfsLink x = new ApfsLink(home, "x", 1, localTime, applications, "owner1", localTime);
+        ApfsLink y = new ApfsLink(code, "y", 1, localTime, a, "owner1", localTime);
+
+    }
+
+    @Test
+    public void testSingletonAPFS(){
+       APFS apfs1 = APFS.getAPFS();
+       APFS apfs2 = APFS.getAPFS();
+
+       assertEquals(apfs1, apfs2);
+    }
+
+    @Test
+    public void capacityTest(){
+        int actual = apfs.getCapacity();
+        assertEquals(1200, actual);
+    }
+}
